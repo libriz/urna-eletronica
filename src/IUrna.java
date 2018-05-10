d82db7e22f293cd0cfd60ccf212970e1757a9e78
libriz <kinivenko@gmail.com>
2018-05-10 11:41:05 -0300

    Update IUrna.java

--- src/IUrna.java
+++ src/IUrna.java
@@ -1,10 +1,10 @@
 
 public interface IUrna {
-    public Candidato incluirCandidato(int id, String nome, String cargo, String partido);
+    public Candidato incluirCandidato(int id, String nome, Cargo cargo, String partido);
     public void setNumeroDeEleitores(int numeroDeEleitores);
     public void setCidade(String cidade);
     public void setEstadoFederativo(String estadoFederativo);
-    public void setZonaEleitoral(String zonaEleitoral);
-    public void setSecaoEleitoral(String secaoEleitoral);
+    public void setZonaEleitoral(int zonaEleitoral);
+    public void setSecaoEleitoral(int secaoEleitoral);
     public void setTurno(int turno);
 }
