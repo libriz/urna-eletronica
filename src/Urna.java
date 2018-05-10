3917a805e3644f5ea1c75407e21a248c9130c69a
libriz <kinivenko@gmail.com>
2018-05-10 11:41:39 -0300

    Update Urna.java

--- src/Urna.java
+++ src/Urna.java
@@ -4,17 +4,17 @@ import java.util.List;
 
 public class Urna implements IUrna{
     public List<Candidato> candidatos = new ArrayList();
-    public int numeroDeEleitores;
-    public String cidade;
     public String estadoFederativo;
-    public String zonaEleitoral;
-    public String secaoEleitoral;
+    public int zonaEleitoral;
+    public int secaoEleitoral;
+    public String cidade;
     public int turno;
+    public int numeroDeEleitores;
     
-    public Candidato incluirCandidato(int id, String nome, String cargo, String partido){
+    public Candidato incluirCandidato(int numero, String nome, Cargo cargo, String partido) throws ErroNoCandidatoException{
         Candidato candidato = new Candidato();
         
-        candidato.setId(id);
+        candidato.setNumero(numero);
         candidato.setNome(nome);
         candidato.setCargo(cargo);
         candidato.setPartido(partido);
@@ -24,27 +24,27 @@ public class Urna implements IUrna{
         return candidato;
     }
     
-    public void setNumeroDeEleitores(int numeroDeEleitores){
-        this.numeroDeEleitores = numeroDeEleitores;
+    public void setEstadoFederativo(String estadoFederativo){
+        this.estadoFederativo = estadoFederativo;
+    }
+    
+    public void setZonaEleitoral(int zonaEleitoral){
+        this.zonaEleitoral = zonaEleitoral;
+    }
+    
+    public void setSecaoEleitoral(int secaoEleitoral){
+        this.secaoEleitoral = secaoEleitoral;
     }
     
     public void setCidade(String cidade){
         this.cidade = cidade;
     }
     
-    public void setEstadoFederativo(String estadoFederativo){
-        this.estadoFederativo = estadoFederativo;
-    }
-    
-    public void setZonaEleitoral(String zonaEleitoral){
-        this.zonaEleitoral = zonaEleitoral;
-    }
-    
-    public void setSecaoEleitoral(String secaoEleitoral){
-        this.secaoEleitoral = secaoEleitoral;
-    }
-    
     public void setTurno(int turno){
         this.turno = turno;
+    }
+    
+    public void setNumeroDeEleitores(int numeroDeEleitores){
+        this.numeroDeEleitores = numeroDeEleitores;
     }
 }
