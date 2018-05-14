14893c24f6d6505d460812d97805a0181aaa46f0
libriz <kinivenko@gmail.com>
2018-05-14 10:02:03 -0300

    Add files via upload

--- src/Principal.java
+++ src/Principal.java
@@ -15,72 +15,92 @@ public class Principal{
             System.out.println("1 cadastrar candidato");
             System.out.println("2 cadastrar urna");
             System.out.println("3 iniciar eleicao");
-            System.out.println("4 encerrar eleicao e mostrar resultado");
-            System.out.println("5 sair");
+            System.out.println("4 encerrar eleicao ou mostrar resultados");
+            System.out.println("5 sair \n");
+            if(eleicao.getTurno()!=null){
+                System.out.println("*** Eleicao em andamento *** \n");
+            }
 
             int opcao=sc.nextInt();
             
             switch(opcao)
             {
                 case 1:
-                    System.out.println("Cadastrar candidato \n");
-                    System.out.println("Digite o numero \n");
-                    int numero=sc.nextInt();
-                    System.out.println("Digite o nome \n");
-                    String nome=sc.next();
-                    System.out.println("Digite o cargo (GOVERNADOR ou DEPUTADO_ESTADUAL)\n");
-                    Cargo cargo=Cargo.valueOf(sc.next().toUpperCase());
-                    System.out.println("Digite o partido \n");
-                    String partido=sc.next();
-                    Candidato candidato = new Candidato(numero, nome, cargo, partido);
-                    //if()
-                    System.out.println("*** Candidato cadastrado! ***\n");
+                    if(eleicao.getTurno()==null){
+                        System.out.println("Cadastrar candidato \n");
+                        System.out.println("Digite o numero \n");
+                        int numero=sc.nextInt();
+                        System.out.println("Digite o nome \n");
+                        String nome=sc.next();
+                        System.out.println("Digite o cargo (GOVERNADOR ou DEPUTADO_ESTADUAL)\n");
+                        Cargo cargo=Cargo.valueOf(sc.next().toUpperCase());
+                        System.out.println("Digite o partido \n");
+                        String partido=sc.next();
+                        Candidato candidato = new Candidato(numero, nome, cargo, partido);
+                        //if()
+                        System.out.println("*** Candidato cadastrado! ***\n");
+                    }else{
+                         System.out.println("*** Eleicao ja foi iniciada! ***\n");
+                    }
                 break;
                 
                 case 2:
-                    System.out.println("Cadastrar urna \n");
-                    System.out.println("Digite o estado federativo \n");
-                    String estadoFederativo=sc.next();
-                    System.out.println("Digite a zona eleitoral \n");
-                    int zonaEleitoral=sc.nextInt();
-                    System.out.println("Digite a secao eleitoral\n");
-                    int secaoEleitoral=sc.nextInt();
-                    System.out.println("Digite a cidade \n");
-                    String cidade=sc.next();
-                    System.out.println("Digite o numero de eleitores \n");
-                    int numeroDeEleitores=sc.nextInt();
-                    Urna urna = new Urna(estadoFederativo, zonaEleitoral, secaoEleitoral, cidade, numeroDeEleitores);
-                    //if()
-                    System.out.println("*** Urna cadastrada! ***\n");
+                    if(eleicao.getTurno()==null){
+                        System.out.println("Cadastrar urna \n");
+                        System.out.println("Digite o estado federativo \n");
+                        String estadoFederativo=sc.next();
+                        System.out.println("Digite a zona eleitoral \n");
+                        int zonaEleitoral=sc.nextInt();
+                        System.out.println("Digite a secao eleitoral\n");
+                        int secaoEleitoral=sc.nextInt();
+                        System.out.println("Digite a cidade \n");
+                        String cidade=sc.next();
+                        System.out.println("Digite o numero de eleitores \n");
+                        int numeroDeEleitores=sc.nextInt();
+                        Urna urna = new Urna(estadoFederativo, zonaEleitoral, secaoEleitoral, cidade, numeroDeEleitores);
+                        //if()
+                        System.out.println("*** Urna cadastrada! ***\n");
+                    }else{
+                         System.out.println("*** Eleicao ja foi iniciada! ***\n");
+                    }
                 break;
                     
                 case 3:
-                    System.out.println("Iniciar eleicao \n");
-                    System.out.println("Digite o turno: (primeiro ou segundo)\n");
-                    String turno=sc.next();
-                    
-                    eleicao = new Eleicao(turno);
-                    
-                    //if()
-                    System.out.println("*** Eleicao iniciada! ***\n");
+                    if(eleicao.getTurno()==null){
+                        System.out.println("Iniciar eleicao \n");
+                        System.out.println("Digite o turno: (primeiro ou segundo)\n");
+                        String turno=sc.next();
+
+                        eleicao = new Eleicao(turno);
+
+                        //if()
+                        System.out.println("*** Eleicao iniciada! ***\n");
+                    }else{
+                         System.out.println("*** Eleicao ja foi iniciada! ***\n");
+                    }
                 break;
                     
                 //votacao
 
                 case 4:
                     if(eleicao.getTurno()!=null){
-                        System.out.println("Encerrar eleicao \n");
+                        System.out.println("encerrar eleicao ou mostrar resultados \n");
                         System.out.println("escolha uma opcao:");
-                        System.out.println("1 resultado geral(todas cidades)");
-                        System.out.println("2 escolher cidade \n");
+                        System.out.println("1 encerrar eleicao");
+                        System.out.println("2 resultado geral(todas cidades)");
+                        System.out.println("3 escolher cidade \n");
 
                         int opcao2=sc.nextInt();
 
                         switch(opcao2){
                             case 1:
+                                    eleicao.setTurno(null);
+                                    System.out.println("*** eleicao encerrada *** \n");
+                            break;
+                            case 2:
                                     System.out.println(eleicao.resultado("todas"));
                             break;
-                            case 2: 
+                            case 3: 
                                 System.out.println("Digite a cidade \n");
                                 String cidade2=sc.next();
 
--- src/Votacao.java
+++ src/Votacao.java
@@ -1,19 +1,48 @@
+
+import java.util.Scanner;
+
 public class Votacao {
 
     public static void main(String[] args) {
+        boolean exit = false;
+                
+        Scanner sc=new Scanner(System.in);
         
-        //1 digitar o numero do candidato
-        //2 voto em branco
-        //3 voto nulo
-        //
-        //
-        //
-        //1
-            //if valido
-                //confirmar?
-            //else
-                //numero invalido
-        //
-        //fim
+        do{
+            //if(){
+                System.out.println("configurar urna / escolha uma opcao:");
+                System.out.println("digitar a cidade de votacao");
+                String cidade=sc.next();
+                //
+                
+            //}else{
+                System.out.println("escolha uma opcao:");
+                System.out.println("1 digitar o numero do candidato");
+                System.out.println("2 voto em branco");
+                System.out.println("3 voto nulo");
+            
+                int opcao=sc.nextInt();
+                
+                switch(opcao){
+                    case 1:
+                        System.out.println("Digite o numero \n");
+                        int numero=sc.nextInt();
+                        
+                        //urna.votar(numero);
+                        exit = true;
+                        break;
+                    
+                    case 2:
+                        //urna.votar(00);
+                        exit = true;
+                        break;
+                        
+                    case 3:
+                        //urna.votar(99);
+                        exit = true;
+                        break;
+            //}
+                }
+        }while (!exit);
     }
 }
